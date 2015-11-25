396d1873e02ea546feb4cdb8d29ef3ef6ce4280b
jmezna <jmezna@gmail.com>
2015-11-25 15:56:45 +0100

    added button on map, added comments

--- app/src/main/java/de/tu_berlin/indornavigation/MapsActivity.java
+++ app/src/main/java/de/tu_berlin/indornavigation/MapsActivity.java
@@ -1,7 +1,8 @@
 package de.tu_berlin.indornavigation;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,7 +12,6 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -44,30 +44,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng berlin = new LatLng(52.512870, 13.326820);
         LatLng mensaCenter = new LatLng(52.509784, 13.326120);
 
+        // add marker in center of mensa and move camera there
         marker = mMap.addMarker(new MarkerOptions().position(mensaCenter).title("Mensa"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mensaCenter, 19));
 
+        // on map click change position of marker
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
 
             @Override
             public void onMapClick(LatLng latLng) {
                 marker.setPosition(latLng);
-                System.out.println("Taped LatLng: " + latLng.latitude + ", " + latLng.longitude);
             }
         });
 
-        LatLngBounds bounds = new LatLngBounds(
-                new LatLng(52.509620, 13.325453),       // South west corner
-                new LatLng(52.509875, 13.327041));      // North east corner
-
+        // add map overlay, set anchor in lower righter corner, set position of anchor, set width
+        // of overlay in meters, rotate overlay clockwise (in degrees)
         GroundOverlayOptions overlayOptions = new GroundOverlayOptions()
                 .image(BitmapDescriptorFactory.fromResource(R.drawable.mensa_eg))
                 .anchor(1, 1).position(new LatLng(52.509490, 13.326278), 51.88f).bearing(26);
 
+        // add overlay to map
         GroundOverlay overlay = mMap.addGroundOverlay(overlayOptions);
+
+    }
+
+    /**
+     * On button clicked, shares position of marker
+     * @param view
+     */
+    public void sharePosition(View view) {
+
+        System.out.println("Marker position is: " + marker.getPosition());
 
     }
 
