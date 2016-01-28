6387a4681d59072a291bca27f892a04f186114f5
Jan Meznaric <jan.meznaric@campus.tu-berlin.de>
2016-01-28 13:19:04 +0100

    show group members

--- app/src/main/java/de/tu_berlin/indoornavigation/MenuActivity.java
+++ app/src/main/java/de/tu_berlin/indoornavigation/MenuActivity.java
@@ -1,5 +1,6 @@
 package de.tu_berlin.indoornavigation;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -15,6 +16,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -75,6 +78,18 @@ public class MenuActivity extends AppCompatActivity implements MyGroupsRecyclerV
     @Override
     public void onListFragmentInteraction(Group item) {
         Log.d(TAG, item.getName());
+
+        Dialog dialog = new Dialog(MenuActivity.this);
+        dialog.setContentView(R.layout.members_list);
+
+        ListView lv = (ListView) dialog.findViewById(R.id.member_names);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout
+                .simple_list_item_1 ,item.getMembers());
+        lv.setAdapter(adapter);
+        dialog.setCancelable(true);
+        dialog.setTitle("ListView");
+        dialog.show();
+
     }
 
 
