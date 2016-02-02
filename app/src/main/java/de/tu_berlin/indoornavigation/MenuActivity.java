22295659ec23f05d2ca41cc13ee4e1e7664cac16
Jan Meznaric <jan.meznaric@campus.tu-berlin.de>
2016-02-02 12:45:48 +0100

    sending friend request

--- app/src/main/java/de/tu_berlin/indoornavigation/MenuActivity.java
+++ app/src/main/java/de/tu_berlin/indoornavigation/MenuActivity.java
@@ -1,17 +1,28 @@
 package de.tu_berlin.indoornavigation;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+import com.estimote.sdk.SystemRequirementsChecker;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,6 +41,37 @@ public class MenuActivity extends AppCompatActivity {
         // initialize hotspots and groups data
         LocationSharingSingleton.getInstance().refreshBeaconsInHotspots();
         UsersGroupsDataSingleton.getInstance().refreshGroupsInfo();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // request location and bluetooth access
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -115,6 +157,74 @@ public class MenuActivity extends AppCompatActivity {
 
         finish();
         System.exit(0);
+    }
+
+    public void showAddFriendDialog(MenuItem item) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Title");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                String email = input.getText().toString();
+
+                Log.d(TAG, email);
+
+                String url = PropertiesSingleton.getInstance().getBackendServerUrl() +
+                        "/companionrequests/";
+
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("userEmail", email); // TODO: remove
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                // Request a string response from the provided URL.
+                JsonObjectRequest putRequest = new JsonObjectRequest(Request.Method.POST, url,
+                        jsonObject,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.d(TAG, "Friend request send. Response is: " + response);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e(TAG, "Friend request send: That didn't work!" + error.toString());
+                    }
+                }) {
+                    @Override
+                    public Map<String, String> getHeaders() {
+                        HashMap<String, String> params = new HashMap<String, String>();
+                        params.put("Cookie", "connect.sid=" + AuthUtils.token);
+
+                        return params;
+                    }
+                };
+
+                VolleyQueueSingleton.getInstance(getApplicationContext()).addToRequestQueue(putRequest);
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+
     }
 
 }
